1d524096907fcaa321b5a8f363557eeff8238726
AlberthWatori <117901024+AlberthWatori@users.noreply.github.com>
2025-05-30 01:06:19 +0700

    Budgetin

---
+++ BUdegtinfix/src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module org.example.budegtinfix {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires java.desktop;
+    requires java.sql;
+
+    opens org.example.budegtinfix to javafx.fxml;
+    exports org.example.budegtinfix;
+}
---
+++ BUdegtinfix/src/main/java/org/example/budegtinfix/HelloController.java
@@ -0,0 +1,32 @@
+package org.example.budegtinfix;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+public class HelloController {
+
+    @FXML
+    private void btnLogin(ActionEvent event) throws IOException {
+        loadScene(event, "Login.fxml");
+    }
+
+    @FXML
+    private void btnSignup(ActionEvent event) throws IOException {
+        loadScene(event, "SignUp.fxml");
+    }
+
+    private void loadScene(ActionEvent event, String fxmlFile) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource(fxmlFile));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
